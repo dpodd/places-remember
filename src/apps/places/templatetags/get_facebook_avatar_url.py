3dc3e27200f653737d9c08bd526b466f4677347b
dpodd <fuzzyworld15@gmail.com>
2020-11-05 15:48:53 +0700

    Configure Facebook avatar to be visible (..in progress..)

---
+++ src/apps/places/templatetags/get_facebook_avatar_url.py
@@ -0,0 +1,19 @@
+from django import template
+from allauth.socialaccount.models import SocialAccount, SocialToken
+
+register = template.Library()
+
+
+@register.simple_tag(takes_context=True, name='get_facebook_avatar_url')
+def generate_facebook_avatar_url(context):
+    user = context['user']
+    socialaccount = SocialAccount.objects.filter(user_id=user.id, provider='facebook').first()
+    token = SocialToken.objects.get(account_id=socialaccount.id)
+    access_token = token.token
+    print(access_token, flush=True)
+    url = "https://graph.facebook.com/{}/picture?width=40&height=40&return_ssl_resources=1&&access_token={}".format(
+                                                                            socialaccount.uid,
+                                                                            access_token
+                                                                            )
+
+    return url
