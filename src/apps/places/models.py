c9fd03bf593d6cf774fadc76846024967d10ef03
dpodd <fuzzyworld15@gmail.com>
2020-11-12 18:12:06 +0700

    Create unique slug for Memory model

--- src/apps/places/models.py
+++ src/apps/places/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.urls import reverse
+from config.utils.unique_slugify import unique_slugify
 
 User = get_user_model()
 
@@ -13,9 +14,15 @@ class Memory(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
+    slug = models.SlugField()
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
-        return reverse('memory_detail', args=[str(self.id)])
+        return reverse('memory_detail', kwargs={'slug': self.slug})
+
+    def save(self, **kwargs):
+        slug_str = "%s" % (self.title,)
+        unique_slugify(self, slug_str)
+        super().save(**kwargs)
--- src/apps/places/urls.py
+++ src/apps/places/urls.py
@@ -3,5 +3,5 @@ from .views import profile_view, memory_view
 
 urlpatterns = [
     path('', profile_view),
-    path('<int:pk>/', memory_view, name='memory_detail'),
+    path('<slug:slug>/', memory_view, name='memory_detail'),
 ]
