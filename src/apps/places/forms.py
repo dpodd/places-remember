06afbb41db07cf19b8b9b9f0f905e5ac142d7ea5
dpodd <fuzzyworld15@gmail.com>
2020-11-12 15:03:47 +0700

    Hide some form fields in browser

--- src/apps/places/forms.py
+++ src/apps/places/forms.py
@@ -5,4 +5,7 @@ from .models import Memory
 class MemoryModelForm(forms.ModelForm):
     class Meta:
         model = Memory
-        fields = ('title', 'description', 'lat', 'lon')
+        fields = ('title', 'description', 'lat', 'lon', 'zoom')
+        widgets = {'lat': forms.HiddenInput(),
+                   'lon': forms.HiddenInput(),
+                   'zoom': forms.HiddenInput()}
