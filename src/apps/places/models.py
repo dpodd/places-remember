cff80407c95cad3e13ef36dcd28913ce0410f953
dpodd <fuzzyworld15@gmail.com>
2020-11-12 16:04:07 +0700

    Delete redundant constraint ('title' field is no unique from now on)

--- src/apps/places/models.py
+++ src/apps/places/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
-import uuid
+from django.urls import reverse
 
 User = get_user_model()
 
@@ -10,9 +10,12 @@ class Memory(models.Model):
     lat = models.DecimalField(max_digits=9, decimal_places=6)
     lon = models.DecimalField(max_digits=9, decimal_places=6)
     zoom = models.IntegerField()
-    title = models.CharField(max_length=100, unique=True)
+    title = models.CharField(max_length=100)
     description = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('memory_detail', args=[str(self.id)])
