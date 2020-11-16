da68421c75db83c062f2ae5b29db4ad2c6567e2b
dpodd <fuzzyworld15@gmail.com>
2020-11-16 22:40:42 +0700

    Write Facebook auth test (WIP)

--- src/apps/places/tests.py
+++ src/apps/places/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 
 from django.db import connection
+from django.urls import reverse
 from allauth.socialaccount.models import SocialApp, SocialAccount
 from django.core.management import call_command
 from django.contrib.sites.models import Site
@@ -8,34 +9,25 @@ from django.contrib.auth import get_user_model
 
 
 
-def oonfig_facebook_provider():
-    config_db_tables()
+def config_facebook_provider():
+    # change to https
+    site = Site.objects.get(id=1)
+    site.domain='https://localhost:8000/'
+    site.name='https://localhost:8000/'
+    site.save()
 
-    def config_db_tables():
-        # change to https
+    # create social app
+    sapp = SocialApp(provider='facebook', name='facebook', client_id='1333592763650736',
+                     secret='')
+    sapp.save()
 
-        # cursor = connection.cursor()
-        # cursor.execute(
-        #     "UPDATE django_site SET domain='https://localhost:8000/' name='https://localhost:8000/' WHERE id=1")
-
-        site = Site.objects.get(id=1)
-        site.domain='https://localhost:8000/'
-        site.name='https://localhost:8000/'
-        site.save()
-        
-
-        # create social app
-        sapp = SocialApp(provider='facebook', name='facebook', client_id='',
-                         secret='')
-        sapp.save()
-
-        sapp.sites.add(1)
+    sapp.sites.add(1)
 
 
 
-def make_initial_migrations():
-    call_command('makemigrations', 'places')
-    call_command('migrate')
+# def make_initial_migrations():
+#     call_command('makemigrations', 'places')
+#     call_command('migrate')
 
 
 def create_test_user():
@@ -46,16 +38,31 @@ def create_test_user():
 
     s_account = SocialAccount(user=user, provider='facebook', uid='')
     s_account.save()
+    return user
+
+# facebook test user id: 104777861428045 , email: open_uvdpjin_user@tfbnw.net
 
 
 class MemoryTestCase(TestCase):
     def setUp(self):
-        make_initial_migrations()
-        create_test_user()
         config_facebook_provider()
+        self.user = create_test_user()
 
-    def test_index_page_healthy(self):
-        # res = self.client.get(reverse('index'))
-        # assertEqual(res.status_code, 200)
-        s1 = SocialApp.objects.get(id=1)
-        self.assertEqual(s1.provider, 'facebook')
+    def test_user_created(self):
+        # s1 = SocialApp.objects.get(id=1)
+        # self.assertEqual(s1.provider, 'facebook')
+        User = get_user_model()
+        u1 = User.objects.filter(username='Dmitry').first()
+        self.assertEqual(u1, self.user)
+
+        # correctly added socialapp_cites
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM socialaccount_socialapp_sites WHERE id=1;")
+        r = cursor.fetchall()
+        print(type(r), flush=True)
+        print(r, flush=True)
+        self.assertEqual(r[0].site_id, 1)
+
+    def test_index_page(self):
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
