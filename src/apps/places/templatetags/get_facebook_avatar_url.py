e81d7a9c61bdb06fb32c9219be400a4c1431d2b1
dpodd <fuzzyworld15@gmail.com>
2020-12-02 07:16:00 +0700

    Make Profile page HTML & CSS layout (WIP: saving progress)

--- src/apps/places/templatetags/get_facebook_avatar_url.py
+++ src/apps/places/templatetags/get_facebook_avatar_url.py
@@ -10,7 +10,7 @@ def generate_facebook_avatar_url(context):
     socialaccount = SocialAccount.objects.filter(user_id=user.id, provider='facebook').first()
     token = SocialToken.objects.get(account_id=socialaccount.id)
     access_token = token.token
-    url = "https://graph.facebook.com/{}/picture?width=40&height=40&return_ssl_resources=1&access_token={}".format(
+    url = "https://graph.facebook.com/{}/picture?width=50&height=50&return_ssl_resources=1&access_token={}".format(
                                                                             socialaccount.uid,
                                                                             access_token
                                                                             )
